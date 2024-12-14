a9fe4e9ff2c9cd09272dcb4914daca8ecac84478
Landi <lizanderbarjamaj@yahoo.com>
2024-12-14 17:16:38 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  experimental: {
+    appDir: true,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
